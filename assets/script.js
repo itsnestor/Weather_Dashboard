5b4420e65ae45283cb8cc01f3775883370aa8af9
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-03-20 21:43:23 -0400

    added fetch api for current weather and 5day

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,49 @@ var currentTemp = $("#temperature");
 var currentHumidity = $("#humidity");
 var currentWindSpeed = $("#windSpeed");
 var currentUVIndex = $("#uvIndex");
+
+var history = JSON.parse(localStorage.getItem("cities")) || [];
+// console.log(history)
+
+// fetch api to get current weather
+var currentWeather = function(city) {
+    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey + "&units=imperial";
+    
+    fetch(currentWeatherUrl).then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            return response.JSON().then(function(data) {
+                console.log(data);
+                displayWeather(data, city);
+            });
+        }
+    });
+}
+
+// fetch api for 5 day forcast
+var fiveDayForcast = function (city) {
+    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    fetch(currentWeatherUrl).then(function(city) {
+        if (response.ok) {
+            console.log(response);
+            return response.JSON().then(function(data) {
+                console.log(data);
+
+                var fiveDayForcast = data.list;
+                var filter = fiveDayForcast.filter(function(day) {
+                    if day.dt_txt.indexOf("12:00:00") !== -1) {
+                        return day;
+                    }
+                });
+                console.log(filter);
+                displayFiveDay(filter);
+            });
+        }
+    });
+}
+
+// function to display current weather
+function displayWeather(data, searchedCity) {
+    var todayDate = moment().format("L");
+}
