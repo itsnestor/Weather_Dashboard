0fab87d364b83c0d881a9211b208d57bf34c4785
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-03-20 20:39:19 -0400

    added moment and jquery libraries, added global var to js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// my api key for openweather api
+var apiKey = "9c776d4121d225d158430a58d8ce793e";
+
+var cityFormEl = $("#cityForm");
+var cityInputEl = $("#searchCity");
+var searchBtn = $("#searchCityBtn");
+var searchHistoryEl = $("#cityHistory");
+var date = $("#date");
+var currentForcast = $("#currentForcast");
+var currentTemp = $("#temperature");
+var currentHumidity = $("#humidity");
+var currentWindSpeed = $("#windSpeed");
+var currentUVIndex = $("#uvIndex");
